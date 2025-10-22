c2d06ff7821fde2e1cd01f3dc6c8c63a792132bc
maryprecious <maryprecious144@gmail.com>
2025-10-22 23:22:45 +0100

    initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:birthday_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      debugShowCheckedModeBanner: false,
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+       routes: {
+        "/": (context)=> HomePage(),
+        // "/home": (context)=> BottomNavigation(),
+        // "/login": (context)=> LoginPage(),
+        // "/signup": (context)=> SignupPage(),
+      },
+    );
+  }
+}
+
+    
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 201, 217, 224),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 100),
+            Image.asset("assets/birthdaycake.png", height: 180, width: 180),
+
+            SizedBox(height: 20),
+            Text(
+              "Happy Birthday",
+              style: TextStyle(fontSize: 28, color: Colors.blue),
+            ),
+
+            Text(
+              "to",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black87
+              ),),
+
+              SizedBox(height: 8),
+              Text(
+              "<IZUCHUKWU AGOGBUA>",
+              style: TextStyle(
+                fontSize: 32,
+                color: const Color.fromARGB(255, 6, 77, 110)
+              ),),
+
+              SizedBox(height: 14),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.celebration,
+                  size: 80,
+                  color: const Color.fromARGB(255, 218, 198, 23),),
+                  SizedBox(width: 13),
+                  Icon(Icons.card_giftcard,
+                  size: 80,
+                  color: const Color.fromARGB(255, 218, 198, 23),),
+                  SizedBox(width: 13),
+                  Icon(Icons.favorite,
+                  size: 80,
+                  color: const Color.fromARGB(255, 218, 198, 23),),
+                  SizedBox(width: 13),
+                  Icon(Icons.star,
+                  size: 80,
+                  color: const Color.fromARGB(255, 218, 198, 23),),
+                ],
+              )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
